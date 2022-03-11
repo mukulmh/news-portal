1130b9df62e919754f4a4221c46649182842eed3
mukulmh <mukulseu@gmail.com>
2022-03-11 14:44:01 +0600

    complete login, registration and  added CSS

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -27,7 +27,7 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
+    @app.route('/')
     def hello():
         return 'Hello, World! Welcome to flask tutorial'
 
